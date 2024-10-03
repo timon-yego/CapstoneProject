cda8fb14bd31cb0ef1d33568a7963efcc58ab111
timon-yego <kipyegotimothy99@gmail.com>
2024-10-03 23:35:25 +0300

    Modified Task Management API

--- task_management_api/task_management/settings.py
+++ task_management_api/task_management/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'tasks',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'tasks.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
--- task_management_api/tasks/serializers.py
+++ task_management_api/tasks/serializers.py
@@ -5,16 +5,18 @@ from .models import Task, CustomUser, TaskCategory, TaskHistory
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'first_name', 'last_name', 'password']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             email=validated_data['email'],
+            username=validated_data['username'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
-            password=validated_data['password']
         )
+        user.set_password(validated_data['password']) 
+        user.save()
         return user
 
 class TaskCategorySerializer(serializers.ModelSerializer):
