c637ae117cb6b2592dae29d87d978d66a5aa7cc4
timon-yego <kipyegotimothy99@gmail.com>
2024-10-04 15:45:19 +0300

    Modified Task Management API

--- task_management_api/task_management/settings.py
+++ task_management_api/task_management/settings.py
@@ -140,3 +140,8 @@ REST_FRAMEWORK = {
     ),
 }
 
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
+}
